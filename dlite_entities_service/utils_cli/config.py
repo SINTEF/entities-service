773b488c4ad554043722a3b860be7272d35893be
Casper Welzel Andersen <casper.andersen@epfl.ch>
2023-04-26 00:41:42 +0200

    Flesh out the CLI
    Map out the initial commands and (almost) finish upload and config.

---
+++ dlite_entities_service/utils_cli/config.py
@@ -0,0 +1,189 @@
+"""config subcommand for dlite-entities-service CLI."""
+# pylint: disable=duplicate-code
+from enum import Enum
+from pathlib import Path
+from typing import Generator, Optional
+
+try:
+    import typer
+except ImportError as exc:
+    raise ImportError(
+        "Please install the DLite entities service utility CLI with "
+        f"'pip install {Path(__file__).resolve().parent.parent.parent.resolve()}[cli]'"
+    ) from exc
+
+from dotenv import dotenv_values, set_key, unset_key
+from rich import print  # pylint: disable=redefined-builtin
+from rich.console import Console
+
+from dlite_entities_service.service.config import CONFIG
+
+ERROR_CONSOLE = Console(stderr=True)
+CLI_DOTENV_FILE = Path(__file__).resolve().parent / ".env"
+SERVICE_DOTENV_FILE = Path(__file__).resolve().parent.parent.parent / ".env"
+
+APP = typer.Typer(
+    name=__file__.rsplit("/", 1)[-1].replace(".py", ""),
+    no_args_is_help=True,
+    invoke_without_command=True,
+)
+
+STATUS = {"use_service_dotenv": False}
+
+
+class ConfigFields(str, Enum):
+    """Configuration options."""
+
+    BASE_URL = "base_url"
+    MONGO_URI = "mongo_uri"
+    MONGO_USER = "mongo_user"
+    MONGO_PASSWORD = "mongo_password"  # nosec
+
+    @classmethod
+    def autocomplete(cls, incomplete: str) -> Generator[tuple[str, str], None, None]:
+        """Return a list of valid configuration options."""
+        for member in cls:
+            if member.value.startswith(incomplete):
+                if member.value not in CONFIG.__fields__:
+                    raise typer.BadParameter(
+                        f"Invalid configuration option: {member.value!r}"
+                    )
+                yield member.value, CONFIG.__fields__[
+                    member.value
+                ].field_info.description
+
+    def is_sensitive(self) -> bool:
+        """Return True if this is a sensitive configuration option."""
+        return self in [ConfigFields.MONGO_PASSWORD]
+
+
+@APP.command(name="set")
+def set_config(
+    key: ConfigFields = typer.Argument(
+        ...,
+        help=(
+            "Configuration option to set. These can also be set as an environment "
+            f"variable by prefixing with {CONFIG.Config.env_prefix!r}."
+        ),
+        show_choices=True,
+        autocompletion=ConfigFields.autocomplete,
+        case_sensitive=False,
+        show_default=False,
+    ),
+    value: Optional[str] = typer.Argument(
+        None,
+        help=(
+            "Value to set. For sensitive values, this will be prompted for if not "
+            "provided."
+        ),
+        show_default=False,
+    ),
+) -> None:
+    """Set a configuration option."""
+    if not value:
+        value = typer.prompt(f"Enter value for {key}", hide_input=key.is_sensitive())
+    if STATUS["use_service_dotenv"]:
+        dotenv_file = SERVICE_DOTENV_FILE
+    else:
+        dotenv_file = CLI_DOTENV_FILE
+    if not dotenv_file.exists():
+        dotenv_file.touch()
+    set_key(dotenv_file, key, value)
+    print(
+        f"Set {key} to sensitive value."
+        if key.is_sensitive()
+        else f"Set {key} to {value}."
+    )
+
+
+@APP.command()
+def unset(
+    key: ConfigFields = typer.Argument(
+        ...,
+        help="Configuration option to unset.",
+        show_choices=True,
+        autocompletion=ConfigFields.autocomplete,
+        case_sensitive=False,
+        show_default=False,
+    ),
+) -> None:
+    """Unset a configuration option."""
+    if STATUS["use_service_dotenv"]:
+        dotenv_file = SERVICE_DOTENV_FILE
+    else:
+        dotenv_file = CLI_DOTENV_FILE
+    if dotenv_file.exists():
+        unset_key(dotenv_file, key)
+    print(f"Unset {key}.")
+
+
+@APP.command()
+def show(
+    reveal_sensitive: bool = typer.Option(
+        False,
+        "--reveal-sensitive",
+        help="Reveal sensitive values. (DANGEROUS! Use with caution.)",
+        is_flag=True,
+        show_default=False,
+    ),
+) -> None:
+    """Show the current configuration."""
+    if STATUS["use_service_dotenv"]:
+        dotenv_file = SERVICE_DOTENV_FILE
+    else:
+        dotenv_file = CLI_DOTENV_FILE
+    if dotenv_file.exists():
+        values = {
+            ConfigFields(key): value
+            for key, value in dotenv_values(dotenv_file).items()
+            if key in ConfigFields.__members__
+        }
+    else:
+        ERROR_CONSOLE.print(f"No {dotenv_file} file found.")
+        raise typer.Exit(1)
+
+    for key, value in values.items():
+        if not reveal_sensitive and key.is_sensitive():
+            value = "***"
+        print(f"[bold]{key}[/bold]: {value}")
+
+
+@APP.callback()
+def main(
+    use_service_dotenv: bool = typer.Option(
+        False,
+        "--use-service-dotenv/--use-cli-dotenv",
+        help=(
+            "Use the .env file also used for the DLite Entities Service or one only "
+            "for the CLI."
+        ),
+        is_flag=True,
+    ),
+    unset_all: bool = typer.Option(
+        False,
+        "--unset-all",
+        help="Unset (remove) all configuration options in dotenv file.",
+        show_default=False,
+        is_flag=True,
+    ),
+) -> None:
+    """Set DLite entities service configuration options."""
+    STATUS["use_service_dotenv"] = use_service_dotenv
+
+    if unset_all:
+        typer.confirm(
+            "Are you sure you want to unset (remove) all configuration options in "
+            f"{'Service' if use_service_dotenv else 'CLI'}-specific .env file?",
+            abort=True,
+        )
+
+        if use_service_dotenv:
+            dotenv_file = SERVICE_DOTENV_FILE
+        else:
+            dotenv_file = CLI_DOTENV_FILE
+
+        if dotenv_file.exists():
+            dotenv_file.unlink()
+            print(f"Removed {dotenv_file}.")
+        else:
+            print(f"No {dotenv_file} file found.")
--- dlite_entities_service/utils_cli/main.py
+++ dlite_entities_service/utils_cli/main.py
@@ -1,5 +1,10 @@
 """Typer CLI for doing DLite entities service stuff."""
+# pylint: disable=duplicate-code
+import json
+import os
+from enum import Enum
 from pathlib import Path
+from typing import TYPE_CHECKING, Optional
 
 try:
     import typer
@@ -9,7 +14,173 @@ except ImportError as exc:
         f"'pip install {Path(__file__).resolve().parent.parent.parent.resolve()}[cli]'"
     ) from exc
 
+import dlite
+import yaml
+from rich import print  # pylint: disable=redefined-builtin
+from rich.console import Console
+
+from dlite_entities_service import __version__
+from dlite_entities_service.service.backend import ENTITIES_COLLECTION
+from dlite_entities_service.utils_cli.config import APP as config_APP
+
+if TYPE_CHECKING:  # pragma: no cover
+    from typing import Any
+
+
+ERROR_CONSOLE = Console(stderr=True)
+
+
+class EntityFileFormats(str, Enum):
+    """Supported entity file formats."""
+
+    JSON = "json"
+    YAML = "yaml"
+    YML = "yml"
+
+
 APP = typer.Typer(
     name="entities-service",
     help="DLite entities service utility CLI",
+    no_args_is_help=True,
+    pretty_exceptions_show_locals=False,
 )
+APP.add_typer(config_APP)
+
+
+def _print_version(value: bool) -> None:
+    """Print version and exit."""
+    if value:
+        print(f"dlite-entities-service version: {__version__}")
+        raise typer.Exit()
+
+
+@APP.callback()
+def main(
+    _: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        help="Show version and exit",
+        is_eager=True,
+        callback=_print_version,
+    ),
+) -> None:
+    """DLite entities service utility CLI."""
+
+
+@APP.command(no_args_is_help=True)
+def upload(
+    filepaths: Optional[list[Path]] = typer.Option(
+        None,
+        "--file",
+        "-f",
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+        help="Path to DLite entity file.",
+        show_default=False,
+    ),
+    directories: Optional[list[Path]] = typer.Option(
+        None,
+        "--dir",
+        "-d",
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        readable=True,
+        resolve_path=True,
+        help=(
+            "Path to directory with DLite entities. All files matching the given "
+            "format(s) in the directory will be uploaded. Subdirectories will be "
+            "ignored."
+        ),
+        show_default=False,
+    ),
+    file_formats: Optional[list[EntityFileFormats]] = typer.Option(
+        [EntityFileFormats.JSON],
+        "--format",
+        help="Format of DLite entity file.",
+        show_choices=True,
+        show_default=True,
+        case_sensitive=False,
+    ),
+) -> None:
+    """Upload a DLite entity."""
+    unique_filepaths = set(filepaths or [])
+    directories = list(set(directories or []))
+    file_formats = list(set(file_formats or []))
+
+    if not filepaths and not directories:
+        ERROR_CONSOLE.print(
+            "[bold red]Error[/bold red]: Either a --file/-f or --dir/-d must be given."
+        )
+        raise typer.Exit(1)
+
+    for directory in directories:
+        for root, _, files in os.walk(directory):
+            unique_filepaths |= set(
+                Path(root) / file
+                for file in files
+                if file.lower().endswith(tuple(file_formats))
+            )
+
+    successes = []
+    for filepath in unique_filepaths:
+        if filepath.suffix[1:].lower() not in file_formats:
+            ERROR_CONSOLE.print(
+                "[bold yellow]Warning[/bold yellow]: File format "
+                f"{filepath.suffix[1:].lower()!r} not supported. Skipping file: "
+                f"{filepath}"
+            )
+            continue
+
+        entity: "dict[str, Any]" = (
+            json.loads(filepath.read_bytes())
+            if filepath.suffix[1:].lower() == "json"
+            else yaml.safe_load(filepath.read_bytes())
+        )
+
+        try:
+            dlite.Instance.from_dict(entity, single=True, check_storages=False)
+        except dlite.DLiteError as exc:  # pylint: disable=redefined-outer-name
+            ERROR_CONSOLE.print(
+                f"[bold red]Error[/bold red]: {filepath} cannot be loaded with DLite. "
+                f"DLite exception: {exc}"
+            )
+            raise typer.Exit(1) from exc
+
+        ENTITIES_COLLECTION.insert_one(entity)
+        successes.append(filepath)
+
+    print(f"Successfully uploaded {len(successes)} entities: {successes}")
+
+
+@APP.command()
+def update():
+    """Update an existing DLite entity."""
+    print("Not implemented yet")
+
+
+@APP.command()
+def delete():
+    """Delete an existing DLite entity."""
+    print("Not implemented yet")
+
+
+@APP.command()
+def get():
+    """Get an existing DLite entity."""
+    print("Not implemented yet")
+
+
+@APP.command()
+def search():
+    """Search for DLite entities."""
+    print("Not implemented yet")
+
+
+@APP.command()
+def validate():
+    """Validate a DLite entity."""
+    print("Not implemented yet")
