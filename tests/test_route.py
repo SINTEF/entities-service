e85c6770c654f637ba268f3b52cf1dfc0afd5f75
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2024-01-04 11:13:04 +0100

    Satisfy new name-tests-test hook

--- tests/test_route.py
+++ tests/test_route.py
@@ -2,16 +2,78 @@
 from __future__ import annotations
 
 import sys
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, NamedTuple
 
 import pytest
-
-from tests.utils import parameterize_get_entities
 
 if TYPE_CHECKING:
     from typing import Any
 
     from fastapi.testclient import TestClient
+
+
+class ParameterizeGetEntities(NamedTuple):
+    """Returned tuple from parameterizing all entities."""
+
+    entity: dict[str, Any]
+    version: str
+    name: str
+
+
+def parameterize_get_entities() -> list[ParameterizeGetEntities]:
+    """Parameterize the test to retrieve all entities."""
+    from pathlib import Path
+
+    import yaml
+
+    def get_version_name(uri: str) -> tuple[str, str]:
+        """Return the version and name part of a uri."""
+        import re
+
+        from dlite_entities_service.service.config import CONFIG
+
+        namespace = str(CONFIG.base_url).rstrip("/")
+
+        match = re.match(
+            rf"^{re.escape(namespace)}/(?P<version>[^/]+)/(?P<name>[^/]+)$", uri
+        )
+        assert match is not None, (
+            f"Could not retrieve version and name from {uri!r}. "
+            "URI must be of the form: "
+            f"{namespace}/{{version}}/{{name}}\n\n"
+            "Hint: Did you (inadvertently) set the base_url to something?"
+        )
+
+        return match.group("version") or "", match.group("name") or ""
+
+    def get_uri(entity: dict[str, Any]) -> str:
+        """Return the uri for an entity."""
+        namespace = entity.get("namespace")
+        version = entity.get("version")
+        name = entity.get("name")
+
+        assert not any(
+            _ is None for _ in (namespace, version, name)
+        ), "Could not retrieve namespace, version, and/or name from test entities."
+
+        return f"{namespace}/{version}/{name}"
+
+    static_dir = (Path(__file__).parent / "static").resolve()
+
+    results: list[ParameterizeGetEntities] = []
+
+    entities: list[dict[str, Any]] = yaml.safe_load(
+        (static_dir / "entities.yaml").read_text()
+    )
+
+    for entity in entities:
+        uri = entity.get("uri") or get_uri(entity)
+
+        version, name = get_version_name(uri)
+
+        results.append(ParameterizeGetEntities(entity, version, name))
+
+    return results
 
 
 @pytest.mark.parametrize(
