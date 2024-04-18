5a0bb91ff0bcf831ca583adf4eb89b9c3b32e716
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2024-04-18 09:41:27 +0200

    Change in CLI root file

--- entities_service/cli/main.py
+++ entities_service/cli/main.py
@@ -27,3 +27,5 @@ for typer_app, typer_app_kwargs in get_subtyper_apps():
 # Add all "leaf"-commands
 for command, commands_kwargs in get_commands():
     APP.command(**commands_kwargs)(command)
+
+print("Change in CLI 'root'")
