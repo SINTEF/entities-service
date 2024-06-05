ce9ce0ebc52ee0503fd4d329b69cedf00d19af96
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2024-06-05 14:29:10 +0200

    Test `--all/-a` option for list entities

--- tests/cli/commands/test_list_entities.py
+++ tests/cli/commands/test_list_entities.py
@@ -32,6 +32,9 @@ def test_list_entities(
 
     If no arguments and options are provided, the command should list all entities in
     the core namespace.
+
+    Note, this will fail if ever a set of test entities are named similarly,
+    but versioned differently.
     """
     from entities_service.models import URI_REGEX, soft_entity
     from entities_service.service.backend import get_backend
@@ -119,7 +122,10 @@ def test_list_entities_namespace(
 ) -> None:
     """Test `entities-service list entities` CLI command.
 
-    With the '--namespace' option.
+    With the 'NAMESPACE' argument.
+
+    Note, this will fail if ever a set of test entities are named similarly,
+    but versioned differently.
     """
     from entities_service.models import URI_REGEX, soft_entity
     from entities_service.service.backend import get_backend
@@ -205,3 +211,127 @@ def test_list_entities_namespace(
         assert f"{name}{version}" in result.stdout.replace(
             " ", ""
         ), CLI_RESULT_FAIL_MESSAGE.format(stdout=result.stdout, stderr=result.stderr)
+
+
+def test_list_entities_all_namespaces(
+    live_backend: bool,
+    cli: CliRunner,
+    list_app: Typer,
+    existing_specific_namespace: str,
+    httpx_mock: HTTPXMock,
+    get_backend_user: GetBackendUserFixture,
+) -> None:
+    """Test `entities-service list entities` CLI command.
+
+    With the '--all/-a' option.
+
+    Note, this will fail if ever a set of test entities are named similarly,
+    but versioned differently.
+    """
+    from entities_service.models import URI_REGEX, soft_entity
+    from entities_service.service.backend import get_backend
+    from entities_service.service.config import CONFIG
+
+    core_namespace = str(CONFIG.base_url).rstrip("/")
+    specific_namespace = f"{core_namespace}/{existing_specific_namespace}"
+
+    backend_user = get_backend_user(auth_role="read")
+    core_backend: MongoDBBackend = get_backend(
+        auth_level="write",
+        settings={
+            "mongo_username": backend_user["username"],
+            "mongo_password": backend_user["password"],
+        },
+        db=None,
+    )
+    core_entities = [soft_entity(**entity) for entity in core_backend.search()]
+    specific_backend: MongoDBBackend = get_backend(
+        auth_level="write",
+        settings={
+            "mongo_username": backend_user["username"],
+            "mongo_password": backend_user["password"],
+        },
+        db=existing_specific_namespace,
+    )
+    specific_entities = [soft_entity(**entity) for entity in specific_backend.search()]
+
+    if not live_backend:
+        # Mock response for listing (valid) namespaces
+        httpx_mock.add_response(
+            url=f"{core_namespace}/_api/namespaces",
+            method="GET",
+            json=[core_namespace, specific_namespace],
+        )
+
+        # Mock response for listing entities from the core namespace
+        httpx_mock.add_response(
+            url=(
+                f"{core_namespace}/_api/entities"
+                f"?namespace=&namespace={existing_specific_namespace}"
+            ),
+            method="GET",
+            json=[
+                entity.model_dump(mode="json", by_alias=True, exclude_unset=True)
+                for entity in [*core_entities, *specific_entities]
+            ],
+        )
+
+    result = cli.invoke(list_app, ("entities", "--all"))
+
+    assert result.exit_code == 0, CLI_RESULT_FAIL_MESSAGE.format(
+        stdout=result.stdout, stderr=result.stderr
+    )
+
+    assert (
+        f"Base namespace: {core_namespace}" in result.stdout
+    ), CLI_RESULT_FAIL_MESSAGE.format(stdout=result.stdout, stderr=result.stderr)
+
+    # We have multiple namespaces, so this line should not appear
+    assert "Specific namespace:" not in result.stdout, CLI_RESULT_FAIL_MESSAGE.format(
+        stdout=result.stdout, stderr=result.stderr
+    )
+
+    # We have multiple namespaces, so this table header should now appear
+    assert "Namespace" in result.stdout, CLI_RESULT_FAIL_MESSAGE.format(
+        stdout=result.stdout, stderr=result.stderr
+    )
+
+    # Ensure number of times namespaces are listed (in short form) in the output
+    # is exactly 1.
+    check_namespaces = {"/": 0, existing_specific_namespace: 0}
+
+    for entity in [*core_entities, *specific_entities]:
+        short_namespace, name, version = None, None, None
+
+        if entity.namespace and entity.name and entity.version:
+            namespace, name, version = entity.namespace, entity.name, entity.version
+            namespace = namespace[len(core_namespace) :]
+            short_namespace = "/" if namespace in ("/", "") else namespace.lstrip("/")
+        else:
+            assert entity.uri
+            match = URI_REGEX.match(str(entity.uri))
+            assert match is not None
+            short_namespace, name, version = (
+                match.group("specific_namespace"),
+                match.group("name"),
+                match.group("version"),
+            )
+
+            if short_namespace is None:
+                short_namespace = "/"
+
+        if name is None or version is None:
+            pytest.fail(
+                f"Name and version could not be extracted from an entity !\n{entity}"
+            )
+
+        assert f"{name}{version}" in result.stdout.replace(
+            " ", ""
+        ), CLI_RESULT_FAIL_MESSAGE.format(stdout=result.stdout, stderr=result.stderr)
+
+        if f"{short_namespace}{name}{version}" in result.stdout.replace(" ", ""):
+            check_namespaces[short_namespace] += 1
+
+    assert list(check_namespaces.values()) == [1] * len(
+        check_namespaces
+    ), CLI_RESULT_FAIL_MESSAGE.format(stdout=result.stdout, stderr=result.stderr)
